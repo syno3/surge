ca0a9a33b14c3fe18dbf7d1320038fac76f18a5e
syno3 <sinotechxvista@gmail.com>
2021-08-17 19:21:02 +0300

    dead for a while

---
+++ Sense/kalman.py
@@ -0,0 +1,16 @@
+#### we build the kalman filter
+
+try:
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import logging
+except Exception as e:
+    logging.error('please fix error {}').format(e)
+    
+class KalmanFilter:
+    def __init__(self) -> None:
+        pass
+    
+    
+KF = KalmanFilter()
