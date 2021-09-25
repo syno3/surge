a7eab4fcd4662b4578c11503e54f86f854ef35c2
syno3 <sinotechxvista@gmail.com>
2021-09-25 19:52:15 +0300

    some conflicting files

--- tests/test_LaneDetection.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-from Percieve.laneDetection import Imagepreporcessing, motionPlanning
-
-class TestImagepreprocessing(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
