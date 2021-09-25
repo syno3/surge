96c5f3bcff2fabc525741de2ea19205e3ba6f15d
syno3 <sinotechxvista@gmail.com>
2021-09-25 20:02:46 +0300

    removed some files

--- Percieve/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name='Lane detection computer vision',
-    ext_modules=cythonize("laneDetection.pyx"),
-    zip_safe=False,
-)
