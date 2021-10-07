3d5a301fcba4c84a0738d60073adad22cc2f3c01
syno3 <sinotechxvista@gmail.com>
2021-10-07 14:48:31 +0300

    start porting code to java

--- drivermonitoring/abstract.py
+++ drivermonitoring/abstract.py
@@ -1,6 +1,14 @@
 # any other feature we would like to be added goes here
 # - we perform face tracking
 # - we monitor can bus driving
+# - we add support for gps data
+# -
+# -
+# -
+# -
+# -
+# -
+# -
 # -
 # -
 # -
@@ -8,6 +16,17 @@
 import cv2
 import numpy as np
 
-class abstract:
+
+class FaceTracking:
+    def __init__(self) -> None:
+        pass
+
+
+class GPS:
+    def __init__(self) -> None:
+        pass
+
+
+class CAN:
     def __init__(self) -> None:
         pass
