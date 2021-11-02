772b2d506489d356904dc923465acf2032d1972b
Festus murimi <sinotechxvista@gmail.com>
2021-11-02 09:47:16 +0300

    more cpp updates

--- drivermonitoring/cpp/src/src/enviroment.cpp
+++ drivermonitoring/cpp/src/src/enviroment.cpp
@@ -1,50 +1,52 @@
 // enviroment file
 // main class
-    // check brightness level
-    // check saturation level
-    // get frames per second
+    // check brightness level (more bugs)
+    // check saturation level (some bugs)
+    // get frames per second (done)
 
-#include "classes.h";
+#include "enviroment.h";
 
-string enviroment::saturation_level(Mat frame){
+saturation_R enviroment::saturation_level(Mat frame){
     // we calculate the mean of hsv[...,2]
-    cvtColor(frame, frame, COLOR_BGR2HSV);
-    float value_S = (float)mean(frame)[1];
+    // returns two variables(string , float value)
+    cvtColor(frame, hsvImage, COLOR_BGR2HSV);
+    float value_S = (float)mean(hsvImage)[1];
     if(value_S == enviroment::_no_saturation){
-        return "no saturation";
+        return { "no saturation", value_S };
     }
     else if(value_S > enviroment::_no_saturation && value_S < enviroment::_little_saturation){
-        return "Minimal saturation";
+        return { "Minimal saturation", value_S };
     }
     else if(value_S > enviroment::_little_saturation && value_S < enviroment::_slight_saturation){
-        return "slight saturation";
+        return { "slight saturation", value_S };
     }
     else if(value_S == enviroment::_slight_saturation && value_S < enviroment::_normal_saturation){
-        return "normal saturation";
+        return { "normal saturation", value_S };
     }
     else (value_S == enviroment::_normal_saturation && value_S < enviroment::_over_saturation);{
-        return "over saturation";
+        return { "over saturation", value_S };
     }
 };
 
-string enviroment::brightness_level(Mat frame) {
+brightness_R enviroment::brightness_level(Mat frame) {
     // we calculate the mean of hsv[...,1]
-    cvtColor(frame, frame, COLOR_BGR2HSV);
-    float value = (float)mean(frame)[2];
+    // returns two variables(string , float value)
+    cvtColor(frame, hsvImage, COLOR_BGR2HSV);
+    float value = (float)mean(hsvImage)[2];
     if (value == enviroment::_no_exposure) {
-        return "no saturation";
+        return { "no saturation", value };
     }
     else if (value > enviroment::_no_exposure && value < enviroment::_little_exposure) {
-        return "Minimal exposure";
+        return {"Minimal exposure", value};
     }
     else if (value > enviroment::_little_exposure && value < enviroment::_slight_exposure) {
-        return "slight exposure";
+        return {"slight exposure", value};
     }
     else if (value == enviroment::_slight_exposure && value < enviroment::_normal_exposure) {
-        return "normal exposure";
+        return {"normal exposure", value};
     }
     else (value == enviroment::_normal_exposure && value < enviroment::_over_exposure); {
-        return "over exposure";
+        return {"over exposure", value};
     }
 };
 
@@ -59,7 +61,8 @@ void main() {
        cap.read(frame);
        resize(frame, frame, Size(), 0.5, 0.5);
        double fps = cap.get(CAP_PROP_FPS);
-       cout << "Frames per second: " << fps << endl;
+       //cout << "Frames per second: " << fps << endl;
+       cout << env.brightness_level(frame).value << endl;
        imshow("video", frame);
        waitKey(1);
    };
---
+++ drivermonitoring/cpp/src/src/face.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "opencv2/highgui/highgui.hpp"
+
+
+// face class
+
+class face {
+public:
+    // variable declaration
+
+    // constructor declarations
+
+};
