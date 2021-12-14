210f63d48d32ecc04b8b549caf1b5457d73daaad
Festus murimi <sinotechxvista@gmail.com>
2021-12-14 19:19:42 +0300

    added material ui icons

--- drivermonitoring/surge-dashboard-app/src/components/pages/dashboard/sidebar.js
+++ drivermonitoring/surge-dashboard-app/src/components/pages/dashboard/sidebar.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import logo from '../resources/sidebarlogo.svg'
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import MapIcon from '@mui/icons-material/Map';
+import InsightsIcon from '@mui/icons-material/Insights';
+import HomeIcon from '@mui/icons-material/Home';
+import FlagIcon from '@mui/icons-material/Flag';
+
 
 
 class SideBar extends Component {
@@ -11,12 +18,42 @@ class SideBar extends Component {
                 <h1>DriverX</h1>
             </div>
             <div className='Dashboard-links'>
-                <div className='Dashboard-li-links'><h1>Overview</h1></div>
-                <div className='Dashboard-li-links'><h1>Statistics</h1></div>
-                <div className='Dashboard-li-links'><h1>Tickets</h1></div>
-                <div className='Dashboard-li-links'><h1>Maps</h1></div>
-                <div className='Dashboard-li-links'><h1>Transactions</h1></div>
-                <div className='Dashboard-li-links'><h1>Settings</h1></div>
+                <div className='Dashboard-li-links'>
+                    <div className='Dashboard-link-icon'>
+                        <HomeIcon style={{fill: "#DDE2FF"}}/>
+                    </div>
+                    <h1>Overview</h1>
+                </div>
+                <div className='Dashboard-li-links'>
+                    <div className='Dashboard-link-icon'>
+                        <InsightsIcon style={{fill: "#DDE2FF"}}/>
+                    </div>
+                    <h1>Statistics</h1>
+                </div>
+                <div className='Dashboard-li-links'>
+                    <div className='Dashboard-link-icon'>
+                        <FlagIcon style={{fill: "#DDE2FF"}}/>
+                    </div>
+                    <h1>Tickets</h1>
+                </div>
+                <div className='Dashboard-li-links'>
+                    <div className='Dashboard-link-icon'>
+                        <MapIcon style={{fill: "#DDE2FF"}}/>
+                    </div>
+                    <h1>Maps</h1>
+                </div>
+                <div className='Dashboard-li-links'>
+                    <div className='Dashboard-link-icon'>
+                        <AccountBalanceWalletIcon style={{fill: "#DDE2FF"}}/>
+                    </div>
+                    <h1>Transactions</h1>
+                </div>
+                <div className='Dashboard-li-links'>
+                    <div className='Dashboard-link-icon'>
+                        <SettingsIcon style={{fill: "#DDE2FF"}}/>
+                    </div>
+                    <h1>Settings</h1>
+                </div>
             </div>
         </div>
         );
